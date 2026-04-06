b536914e76dca66159b4aac426d2a07ff58ca9be
Arfatul Shaikh <motulshaikh@gmail.com>
2026-04-06 19:12:35 +0530

    fix: Fix template syntax error in factory_detail.html
    - Replace inline conditional with proper Django if/else tag
    - Fix 'Could not parse remainder' error for view count display

--- blog/views.py
+++ blog/views.py
@@ -55,6 +55,7 @@ from location.models import Country, State, City, District, Region
 from Karkahan.views import process_hierarchical_fields
 from Accounts.decorators import email_verified_required
 
+
 class BlogPostListView(ListView):
     model = BlogPost
     template_name = 'blog/post_list.html'
